b239b0facbddd5204358acab015b3326a33842b5
大辉 <dbstt@qq.com>
2023-06-15 17:41:06 +0800

    config change

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: 大辉 dbstt@qq.com
  * @Date: 2023-06-14 14:04:58
  * @LastEditors: 大辉 dbstt@qq.com
- * @LastEditTime: 2023-06-15 17:36:20
+ * @LastEditTime: 2023-06-15 17:40:02
  * @FilePath: /mengnan/vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,9 +14,9 @@ export default defineConfig({
   build:{
     outDir: 'lib',
     lib: {
-      entry: resolve(__dirname, 'lib/index.js'),
+      entry: resolve(__dirname, 'src/main.ts'),
       name: 'index',
-      // fileName: (format) => `mengnan.${format}`
+      fileName: (format) => `index.${format}.js`
     }
   },
 });
