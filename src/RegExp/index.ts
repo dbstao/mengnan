6973ec8a12549a6dd829b4665758969a1f9ed9ee
大辉 <dbstt@qq.com>
2023-06-15 11:07:14 +0800

    add verify

--- src/RegExp/index.ts
+++ src/RegExp/index.ts
@@ -2,10 +2,13 @@
  * @Author: 大辉 dbstt@qq.com
  * @Date: 2023-06-14 17:28:46
  * @LastEditors: 大辉 dbstt@qq.com
- * @LastEditTime: 2023-06-15 10:41:11
+ * @LastEditTime: 2023-06-15 11:01:40
  * @FilePath: /mengnan/src/RegExp/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-export function ferifyPhoneNumber(){
-
+import {CNMobilePhone} from './verifyEnum'
+export function verifyMobilePhone(number: string | number, country = 'cn'){
+    if(country === 'cn'){
+        return CNMobilePhone.test(number.toString())
+    }
 }
---
+++ src/RegExp/verifyEnum.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: 大辉 dbstt@qq.com
+ * @Date: 2023-06-15 10:52:05
+ * @LastEditors: 大辉 dbstt@qq.com
+ * @LastEditTime: 2023-06-15 11:01:42
+ * @FilePath: /mengnan/src/RegExp/verifyEnum.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export const CNMobilePhone = /^1[3-9]\d{9}$/;
